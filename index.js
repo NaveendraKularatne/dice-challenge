5b9c2848edf5c89e86dae28a08759eb3150d5c1e
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-10-17 18:24:06 +0530

    Improve the method

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var randomNumTwo = Math.random();
 randomNumOne = Math.floor (randomNumOne*6) + 1;
 randomNumTwo = Math.floor (randomNumTwo*6) + 1;
 
+//Another way / more productive way
+var randomDiceImage = "dice" + randomNumOne + ".png"; //dice1.png - dice2.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice2.png
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomImageSource2 = "images/dice" + randomNumTwo + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
+
 if (randomNumOne > randomNumTwo) {
 	//player one wins
 	document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
@@ -16,7 +24,7 @@ if ( randomNumOne === randomNumTwo) {
 	document.querySelector("h1").innerHTML = "Draw!";
 }
 
-if (randomNumOne === 1) {
+/*if (randomNumOne === 1) {
 	document.getElementById('img1').src='images/dice1.png';
 } else if (randomNumOne === 2) {
 	document.getElementById('img1').src='images/dice2.png';
@@ -42,5 +50,4 @@ if (randomNumTwo === 1) {
 	document.getElementById('img2').src='images/dice5.png';
 } else if (randomNumTwo === 6) {
 	document.getElementById('img2').src='images/dice6.png';
-}
-
+}*/
